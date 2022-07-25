41f78806de72513149ff4681936392f59b69267e
André Felipe Molinari Melo <molinari.andref@gmail.com>
2022-07-25 15:41:57 -0300

    Finalizado Aula_05

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -92,18 +92,28 @@ describe('Central de Atendimento ao Cliente TAT', () => {
   });
 
   //Aula 04 - Get RADIO BUTTON
-  it.only('marca o tipo de atendimento "Feedback"', () => {
+  it('marca o tipo de atendimento "Feedback"', () => {
     cy.get('input[type="radio"][value="feedback"]')
       .check()
       .should('have.value', 'feedback');
   });
 
-  it.only('marca cada tipo de atendimento', () => {
+  it('marca cada tipo de atendimento', () => {
     cy.get('input[type="radio"]')
       .should('have.length', 3)
       .each(($radio) => {
         cy.wrap($radio).check();
         cy.wrap($radio).should('be.checked');
       });
+  });
+
+  //Aula 05 - Get CHECKBOX
+  it('marca ambos checkboxes, depois desmarca o último', () => {
+    cy.get('input[type="checkbox"]')
+      .check()
+      .should('be.checked')
+      .last()
+      .uncheck()
+      .should('not.be.checked');
   });
 });
