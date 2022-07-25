e27fa262c24a9dd235c17cf0add6679d3ad25d9b
André Felipe Molinari Melo <molinari.andref@gmail.com>
2022-07-25 15:58:02 -0300

    Finalizado Aula_06

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -116,4 +116,34 @@ describe('Central de Atendimento ao Cliente TAT', () => {
       .uncheck()
       .should('not.be.checked');
   });
+
+  //Aula 06 - Upload files
+  it.only('seleciona um arquivo da pasta fixtures', () => {
+    cy.get('input[type="file"]#file-upload')
+      .should('not.have.value')
+      .selectFile('./cypress/fixtures/example.json')
+      .should(($input) => {
+        expect($input[0].files[0].name).to.equal('example.json');
+      });
+  });
+
+  it.only('seleciona um arquivo simulando um drag-and-drop', () => {
+    cy.get('input[type="file"]#file-upload')
+      .should('not.have.value')
+      .selectFile('./cypress/fixtures/example.json', {
+        action: 'drag-drop',
+      })
+      .should(($input) => {
+        expect($input[0].files[0].name).to.equal('example.json');
+      });
+  });
+
+  it.only('seleciona um arquivo utilizando uma fixture para qual foi dada um alias', () => {
+    cy.fixture('example.json').as('sampleFile');
+    cy.get('input[type="file"]#file-upload')
+      .selectFile('@sampleFile')
+      .should(($input) => {
+        expect($input[0].files[0].name).to.equal('example.json');
+      });
+  });
 });
